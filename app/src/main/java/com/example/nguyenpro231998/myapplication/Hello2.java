348b66a6828a7664c5dc2afa266afbd3b0b043ca
ultraman231998 <anhnguyen23109898@gmail.com>
2018-08-26 10:39:54 +0700

    thêm 1 class Hello2

---
+++ app/src/main/java/com/example/nguyenpro231998/myapplication/Hello2.java
@@ -0,0 +1,8 @@
+package com.example.nguyenpro231998.myapplication;
+
+import android.content.pm.ApplicationInfo;
+import android.support.v7.app.AppCompatActivity;
+
+public class Hello2 extends AppCompatActivity{
+
+}
